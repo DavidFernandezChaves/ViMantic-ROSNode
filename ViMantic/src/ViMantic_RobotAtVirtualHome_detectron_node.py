2c1ffd14f10b7d6d1ebd2f483a86c94b4581373c
matez <josematez@uma.es>
2021-05-26 18:21:01 +0200

    2.1.0

--- ViMantic/src/ViMantic_RobotAtVirtualHome_detectron_node.py
+++ ViMantic/src/ViMantic_RobotAtVirtualHome_detectron_node.py
@@ -5,193 +5,257 @@ import time
 import cv2
 import detectron2_ros.msg
 import numpy as np
+import open3d as o3d
 import rospy
 import tf2_geometry_msgs
 import tf2_ros
+from math import pi, sin, cos
 from cv_bridge import CvBridge, CvBridgeError
-from geometry_msgs.msg import PoseStamped, Point, Vector3, PoseWithCovariance
-from sensor_msgs.msg import Image, CompressedImage
-from vimantic.msg import SemanticObject, SemanticObjectArray
-from vision_msgs.msg import ObjectHypothesis
+from tf.transformations import quaternion_from_euler
 
+from geometry_msgs.msg import PoseStamped, Point, Vector3, Pose, Quaternion
+from sensor_msgs.msg import Image, CompressedImage
+from vimantic.msg import Detection, DetectionArray, ObjectHypothesis
 
 class ViManticNode(object):
     def __init__(self):
+
+        # Control and flags
+        self._publish_rate = 10
+        self._image_counter = 0
+        self._start_time = 0
+        self._tries = 0
+        self._max_tries = 10
+        self._flag_processing = False
+        self._flag_cnn = False
+
         rospy.logwarn("Initializing")
         # ROS Parameters
         self.image_topic = self.load_param('~topic_virtualCameraRGBD', "ViMantic/virtualCameraRGBD")
-        self.semantic_topic = self.load_param('~topic_result', 'ViMantic/SemanticObjects')
-        self.cnn_topic = self.load_param('~topic_cnn')
+        self.semantic_topic = self.load_param('~topic_result', 'ViMantic/Detections')
+        self.cnn_topic = self.load_param('~topic_cnn', 'detectron2_ros/result')
         self.image_toCNN = self.load_param('~topic_republic', 'ViMantic/ToCNN')
+        self.n_steps_fitting = self.load_param('~n_steps_fitting', 180)
         self.debug = self.load_param('~debug', False)
 
-        # Camera calibration
-        self.cx = 320
-        self.cy = 240
-        self.fx = 457.1429
-        self.fy = 470.5882
+        # Orientation Fitting Variables
+        theta = (180.0 / self.n_steps_fitting) * pi / 180.0
+        self._R = np.asarray([[cos(theta), -sin(theta), 0], [sin(theta), cos(theta), 0], [0, 0, 1]])
+
+        # Camera Calibration
+        self._cx = 320
+        self._cy = 240
+        self._fx = 457.1429
+        self._fy = 470.5882
+        self._depth_range = 15
 
         # General Variables
-        self._bridge = CvBridge()
-        self._tfBuffer = tf2_ros.Buffer()
-        self._image_counter = 0
         self._last_msg = None
-        self._waiting_cnn = False
-        self._tries = 0
+        self._last_cnn_result = None
+        self._image_r = None
+        self._image_c = None
 
-        # Publisher
-        self._pub_result = rospy.Publisher(self.semantic_topic, SemanticObjectArray, queue_size=10)
-        self._pub_repub = rospy.Publisher(self.image_toCNN, Image, queue_size=1)
+        # Publishers
+        self._pub_result = rospy.Publisher(self.semantic_topic, DetectionArray, queue_size=10)
+        self._pub_processed_image = rospy.Publisher(self.image_toCNN, Image, queue_size=1)
 
         if self.debug:
             self._pub_pose = rospy.Publisher('ViMantic/detectedObject', PoseStamped, queue_size=1)
 
         # Subscribers
         rospy.Subscriber(self.cnn_topic, detectron2_ros.msg.Result, self.callback_new_detection)
-        rospy.Subscriber(self.image_topic, CompressedImage, self.callbackVirtualImage, queue_size=10)
+        rospy.Subscriber(self.image_topic, CompressedImage, self.callback_virtual_image, queue_size=10)
 
+        # Handlers
+        self._bridge = CvBridge()
+        self._tfBuffer = tf2_ros.Buffer()
         tf2_ros.TransformListener(self._tfBuffer)
-        self.start_time = 0
+
         rospy.logwarn("Initialized")
 
     def run(self):
 
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(self._publish_rate)
 
         while not rospy.is_shutdown():
-            # Republish last img
-            if self._waiting_cnn and self._tries > 200:
-                self._pub_repub.publish(self._bridge.cv2_to_imgmsg(self._last_msg[1], 'rgb8'))
-                self._waiting_cnn = False
-                rospy.logwarn("[ViMantic] CNN does not respond, trying again.")
-            else:
-                self._tries += 1
 
-            rate.sleep()
+            # Extracting and publishing detected objects
+            if self._flag_processing and self._flag_cnn:
 
-    def callbackVirtualImage(self, img_msg):
+                # Obtain 3D coordinates in meters of each pixel
+                z = self._last_msg[2] * self._depth_range
+                x = ((self._cx - self._image_c) * z / self._fx)
+                y = ((self._cy - self._image_r) * z / self._fy)
 
-        if not self._waiting_cnn:
-            transform = self._tfBuffer.lookup_transform("map",
-                                                        img_msg.header.frame_id,  # source frame
-                                                        rospy.Time(0))  # get the tf at first available time
+                # Initializing result message
+                detections = DetectionArray()
+                detections.header = self._last_msg[0]
+                detections.header.frame_id = "/map"
 
-            np_arr = np.fromstring(img_msg.data, np.uint8)
-            im = cv2.imdecode(np_arr, -1)
-            img_rgb = cv2.cvtColor(im[:, :, :3], cv2.COLOR_RGB2BGR)
+                # Initializing console message
+                obj_stringstring = 'Detected:'
 
-            img_depth = np.divide(im[:, :, 3], 255.0)
+                for idx in range(len(self._last_cnn_result.class_names)):
 
-            self._last_msg = [img_msg.header, img_rgb, img_depth, transform]
-            self._pub_repub.publish(self._bridge.cv2_to_imgmsg(self._last_msg[1], 'rgb8'))
-            self._tries = 0
-            self._waiting_cnn = True
-            if self.start_time == 0:
-                self.start_time = time.time()
-
-    def callback_new_detection(self, result_cnn):
-        if self._waiting_cnn:
-            self._image_counter = self._image_counter + 1
-            if (self._image_counter % 11) == 10:
-                rospy.loginfo("Images detected per second=%.2f",
-                              float(self._image_counter) / (time.time() - self.start_time))
-
-            if len(result_cnn.class_names) > 0:
-                data_header = self._last_msg[0]
-                img_depth = self._last_msg[2]
-                data_transform = self._last_msg[3]
-
-                # Transform the value of each px to m by acquiring a cloud of points
-                img_depth = img_depth * 15
-
-                rows, cols = img_depth.shape
-                c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
-
-                z = img_depth
-                x = ((self.cx - c) * z / self.fx)
-                y = ((self.cy - r) * z / self.fy)
-
-                # Cut out every object from the point cloud and build the result.
-                result = SemanticObjectArray()
-
-                result.header = data_header
-                result.header.frame_id = "/map"
-                objstring = 'Detected:'
-
-                for i in range(len(result_cnn.class_names)):
-
-                    semanticObject = SemanticObject()
+                    detection = Detection()
                     det = ObjectHypothesis()
-                    det.id = result_cnn.class_names[i]
-                    det.score = result_cnn.scores[i]
-                    semanticObject.scores.append(det)
-
+                    det.id = self._last_cnn_result.class_names[idx]
+                    det.score = self._last_cnn_result.scores[idx]
+                    detection.scores.append(det)
+                    detection.origin = self._last_msg[4]
 
                     try:
-                        mask = (self._bridge.imgmsg_to_cv2(result_cnn.masks[i]) == 255)
+                        mask = (self._bridge.imgmsg_to_cv2(self._last_cnn_result.masks[idx]) == 255)
+
                     except CvBridgeError as e:
                         print(e)
                         continue
 
-                    if x.shape != mask.shape:
-                        return
-
+                    # Get detected object point cloud
                     x_ = x[mask]
                     y_ = y[mask]
                     z_ = z[mask]
 
+                    # Statistics from Z data
+                    mean = np.mean(z_)
+                    std = np.std(z_)
+
                     # Bandpass filter with Z data
-                    top_margin = (z_.max() - z_.min()) * 0.9 + z_.min()
-                    bottom_margin = (z_.max() - z_.min()) * 0.1 + z_.min()
+                    top_margin = mean + 2.0 * std
+                    bottom_margin = mean - 2.0 * std
+                    filtered_mask = np.logical_and(z_ > bottom_margin, z_ < top_margin)
 
-                    mask2 = np.logical_and(z_ > bottom_margin, z_ < top_margin)
-                    # mask2 = z_ < top_margin
+                    # Obtain filtered point cloud
+                    point_cloud = np.array([x_[filtered_mask].reshape(-1),
+                                            y_[filtered_mask].reshape(-1),
+                                            z_[filtered_mask].reshape(-1)]).T
 
-                    x_ = x_[mask2]
-                    y_ = y_[mask2]
-                    z_ = z_[mask2]
+                    pcd = o3d.geometry.PointCloud()
+                    pcd.points = o3d.utility.Vector3dVector(point_cloud)
 
-                    if len(x_) == 0:
-                        return
 
-                    scale_x = x_.max() - x_.min()
-                    scale_y = y_.max() - y_.min()
-                    scale_z = np.std(z_)
 
-                    # semanticObject.size = Vector3(scale_z, scale_x , scale_y)
-                    semanticObject.size = Vector3(scale_x, scale_z, scale_y)
 
-                    # Calculate the center
-                    x_center = int(result_cnn.boxes[i].x_offset + result_cnn.boxes[i].width / 2)
-                    y_center = int(result_cnn.boxes[i].y_offset + result_cnn.boxes[i].height / 2)
-                    z_center = scale_z / 2 + z_.min()
 
-                    # Transformed the center of the object to the map reference system
+                    best_angle = 0
+                    best_obb = pcd.get_axis_aligned_bounding_box()
+                    volume = best_obb.volume()
+
+                    for i in range(self.n_steps_fitting):
+
+                        pcd.rotate(self._R)
+
+                        obb = pcd.get_axis_aligned_bounding_box()
+
+                        if obb.volume() < volume:
+                            best_obb = obb
+                            volume = obb.volume()
+                            best_angle = (i + 1) * 180.0 / self.n_steps_fitting
+
+                    # Size
+                    detection.size = Vector3(*best_obb.get_extent())
+
+                    # Position
+                    position = best_obb.get_center()
+
+                    # Transform the center of the object to the map reference system
                     p1 = PoseStamped()
-                    p1.header = data_header
-                    p1.pose.position = Point(z_center, x[y_center, x_center], y[y_center, x_center])
+                    p1.header = self._last_msg[0]
+                    p1.pose.position = Point(position[2], position[0], position[1])
                     p1.pose.orientation.w = 1.0  # Neutral orientation
-                    ans = tf2_geometry_msgs.do_transform_pose(p1, data_transform)
+                    pose_result = tf2_geometry_msgs.do_transform_pose(p1, self._last_msg[3])
 
-                    semanticObject.pose = PoseWithCovariance()
-                    semanticObject.pose.pose = ans.pose
+                    pose_result.pose.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, best_angle*pi/180.0, axes = 'rxyz'))
 
-                    self._pub_pose.publish(ans)
+                    detection.pose = pose_result.pose
+                    self._pub_pose.publish(pose_result)
 
-                    result.semanticObjects.append(semanticObject)
-                    objstring = objstring + ' ' + det.id + ', p=%.2f.' % (
-                        det.score)
+                    detections.detections.append(detection)
+                    obj_stringstring = obj_stringstring + ' ' + det.id + ', p=%.2f.' % det.score
 
-                self._pub_result.publish(result)
-                rospy.loginfo(objstring)
+                self._pub_result.publish(detections)
+                rospy.loginfo(obj_stringstring)
 
-            self._waiting_cnn = False
+                self._flag_cnn = False
+                self._flag_processing = False
 
+            # CNN does not respond.
+            elif self._flag_processing and not self._flag_cnn:
+                # Skipping frame.
+                if self._tries > self._max_tries:
+                    self._flag_processing = False
+                    rospy.logwarn("[ViMantic] CNN does not respond, skipping frame.")
+
+                # Awaiting CNN to process the last image
+                else:
+                    self._pub_processed_image.publish(self._bridge.cv2_to_imgmsg(self._last_msg[1], 'rgb8'))
+                    self._tries += 1
+
+            rate.sleep()
+
+    def callback_virtual_image(self, img_msg):
+
+        if not self._flag_processing:
+            transform = self._tfBuffer.lookup_transform("map",
+                                                        img_msg.header.frame_id,  # source frame
+                                                        rospy.Time(0))  # get the tf at first available time
+
+            img_rgb, img_depth = self.decode_image_from_unity(img_msg.data)
+
+            try:
+                trans = self._tfBuffer.lookup_transform('map', img_msg.header.frame_id, rospy.Time())
+                origin = Pose()
+                origin.position.x = trans.transform.translation.x
+                origin.position.y = trans.transform.translation.y
+                origin.position.z = trans.transform.translation.z
+                origin.orientation = trans.transform.rotation
+
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                rospy.logwarn("Failing to retrieve robot pose")
+
+            self._last_msg = [img_msg.header, img_rgb, img_depth, transform, origin]
+            self._pub_processed_image.publish(self._bridge.cv2_to_imgmsg(self._last_msg[1], 'rgb8'))
+            self._tries = 0
+
+            if self._start_time == 0:
+                self._start_time = time.time()
+
+            if self._image_c is None and self._image_r is None:
+                # Generate a meshgrid where each pixel contains its pixel coordinates
+                rows, cols = img_depth.shape
+                self._image_c, self._image_r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+            self._flag_processing = True
+
+    def callback_new_detection(self, result_cnn):
+
+        if self._flag_processing and not self._flag_cnn:
+            self._image_counter = self._image_counter + 1
+            if (self._image_counter % 11) == 10:
+                rospy.loginfo("Images detected per second=%.2f",
+                              float(self._image_counter) / (time.time() - self._start_time))
+
+            if len(result_cnn.class_names) > 0:
+                self._last_cnn_result = result_cnn
+                self._flag_cnn = True
+
+            else:
+                self._flag_processing = False
+
+    # Static Methods
     @staticmethod
     def load_param(param, default=None):
         new_param = rospy.get_param(param, default)
         rospy.loginfo("[ViMantic] %s: %s", param, new_param)
         return new_param
+
+    @staticmethod
+    def decode_image_from_unity(unity_img):
+        np_arr = np.fromstring(unity_img, np.uint8)
+        im = cv2.imdecode(np_arr, -1)
+        img_rgb = cv2.cvtColor(im[:, :, :3], cv2.COLOR_RGB2BGR)
+        img_depth = np.divide(im[:, :, 3], 255.0)
+
+        return img_rgb, img_depth
 
 
 def main(argv):
